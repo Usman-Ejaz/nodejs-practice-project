bf217a45a8c96cd3cd75ee1f59de07330af7adbf
Usman Ejaz <usman.ejaz@nxb.com.pk>
2022-08-03 16:22:39 +0500

    updates

--- app/models/User.js
+++ app/models/User.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    // _id: {
-    //     type: mongoose.Schema.Types.ObjectId,
-    // },
     name: {
         type: String,
         required: true,
